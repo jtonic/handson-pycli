f9fc9c3f822e63fd97e0a9a78a944828a0231b23
jtonic <antonel.pazargic@gmail.com>
2020-07-30 13:56:36 +0300

    [python] Some changes in setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
 from setuptools import setup, find_packages
 setup(
     name='pycli',
-    version='0.1.1',
+    version='0.1.2',
     description='Py cli project',
     author='Antonel-Ernest Pazargic',
     author_email='antonel.pazargic@gmail.com',
+    url='https://github.com/jtonic/handson-pycli',
+    license='MIT',
     packages=find_packages(where='src'),
     package_dir={'': 'src'},
     python_requires='>=3.6, <4',
@@ -18,5 +20,10 @@ setup(
       'console_scripts': [
         'pycli=pycli:main',
       ],
-    }
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
 )
